f310d9ee2859f8b7241bbadd7ecc9d890a892086
Rory-luo <55289804+Rory-luo@users.noreply.github.com>
2023-11-01 19:10:28 +0800

    Update main.py
    Changed the way to own the weather's informations

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def get_weather():
             lowest = data['main']['temp_min']
             airQuality = '优'
             airData = data['main']['pressure']
-            return {'weather': weather, 'temperature': temperature, 'humidity': humidity, 'highest': highest, 'lowest': lowest, 'airQuality': airQuality, 'airData': airData}
+            return {'weather': weather, 'temperature': temperature, 'humidity': humidity, 'high': highest, 'low': lowest, 'airQuality': airQuality, 'airData': airData}
         elif response.status_code == 401:
             print("API key is invalid or unauthorized. Please check your API key.")
             return exit(502)
